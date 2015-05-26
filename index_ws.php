57f3882c4adc8bb52645d5478a133ff7be3952b6
uncovery <github@uncovery.me>
2015-05-26 17:50:54 +0800

    added comments

--- index_ws.php
+++ index_ws.php
@@ -1,6 +1,8 @@
 <?php
 global $UMC_USER, $UMC_ENV, $UMC_SETTING;
 require_once(__DIR__ . '/core_include.php');
+
+// we are coming from websend, so include wordpress
 require_once($UMC_SETTING['path']['wordpress'] . '/wp-load.php');
 
 if (!$UMC_ENV) {
