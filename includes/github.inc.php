4e07dcbb8045d09caeac2c1f653d7c196683ef17
uncovery <minecraft@uncovery.me>
2015-06-12 12:09:50 +0800

    initial commit for github link

--- includes/github.inc.php
+++ includes/github.inc.php
@@ -4,6 +4,9 @@ function umc_github_link() {
     $secret = 'IY8uSgfq3HWl60jiOzgC';
     echo "Hello World!";
 
-    XMPP_ERROR_trigger("text");
+    $foo = file_get_contents("php://input");
+
+    $value = var_export(json_decode($foo, true), true);
+    XMPP_ERROR_trigger("$value");
 
 }
