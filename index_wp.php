69485b2353b283a078b8c63b826eca8855c738cd
uncovery <minecraft@uncovery.me>
2015-06-22 17:26:11 +0800

    do not process any functions if we have a 404 error;

--- index_wp.php
+++ index_wp.php
@@ -1,5 +1,11 @@
 <?php
 global $UMC_USER, $UMC_ENV;
+
+// do not process any functions if we have a 404 error;
+if (function_exists('is_404') && is_404()) {
+    return;
+}
+
 require_once(__DIR__ . '/core_include.php');
 
 $s_get = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
@@ -17,13 +23,13 @@ if (!$UMC_ENV) {
 
 /**
  * Verify that an externally called function is actually a vaild, no-risk function call.
- * 
+ *
  * @global type $UMC_FUNCTIONS
  * @param type $function
  */
 function umc_function_call($function) {
     global $UMC_FUNCTIONS;
-    
+
     // we check if the function is in the list of known functions
     if (!isset($UMC_FUNCTIONS[$function])) {
         // if not, send a warning so that it can be included for the future
