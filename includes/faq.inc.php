64fb54a7c420aee9fb8acfb0134ba3b63f159452
uncovery <minecraft@uncovery.me>
2015-07-03 11:39:08 +0800

    faq function improvements

--- includes/faq.inc.php
+++ includes/faq.inc.php
@@ -7,7 +7,8 @@
 
 // we want to have a fixed, unique ID for every FAQ to be able to link to it
 // we can have several categories
-$FAQ = array(
+global $UMC_FAQ;
+$UMC_FAQ = array(
     1 => array(
         'question' => 'I built something amazing for the city! Can you copy it over?',
         'answer' => 'Do not build something because you think the city needs it. Do not ask me to copy things because “you built it for the city”.
@@ -17,14 +18,15 @@ $FAQ = array(
     ),
     2 => array(
         'question' => 'I want to change my Minecraft Username. What do I have to do?',
-        'answer' => '1) remove all your inventory from your body in all worlds – deposit it or put it into normal chests, not ender chests
-            2) Remove all your contents from all ender chests in all worlds
-            3) Use your XP, it will be gone, too
-            4) Log off from the website and the game
-            5) change your name with mojang
-            6) Login at the game
-            7) Login at the website
-            8) check if the “welcome” message on the website changed',
+        'answer' => '<ul>
+            <li>Remove all your inventory from your body in all worlds – deposit it or put it into normal chests, not ender chests</li>
+            <li>Remove all your contents from all ender chests in all worlds</li>
+            <li>Use your XP, it will be gone, too</li>
+            <li>Log off from the website and the game</li>
+            <li>change your name with mojang</li>
+            <li>Login at the game</li>
+            <li>Login at the website</li>
+            <li>check if the “welcome” message on the website changed</li></ul>',
         'categories' => array('account'),
     ),
     3 => array(
@@ -51,7 +53,7 @@ $FAQ = array(
         'categories' => array('account', 'lots'),
     ),
     7 => array(
-        'question' => 'I got bored with my protected lot and would like to restart from scratch, can you reset my lot? Or can I switch from Empire to Flatlands?',
+        'question' => 'I would like to restart from scratch, can you reset my lot? Or can I switch from Empire to Flatlands?',
         'answer' => 'No. If I start doing this, I will have people migrating from one lot to the other and I have all the work.
             You select a lot, you keep it. I allow people to switch from Flatlands to Empire as an exception, but only in one way – once.
             I cannot keep track of people switching forward and backward. Every switch is work for me, and I have better things to do than to deal with your indecisiveness.',
@@ -90,25 +92,25 @@ $FAQ = array(
         'categories' => array('blocks', 'problems'),
     ),
     13 => array(
-        'questions' => 'Can I allow others to build inside my lot?',
+        'question' => 'Can I allow others to build inside my lot?',
         'answer' => 'Yes, you can do so yourself. Please see the <a href="http://uncovery.me/about-this-server/user-levels/">user levels</a> page for instructions.',
         'categories' => array('commands', 'lots'),
     ),
     14 => array(
-        'questions' => 'Can the admin give me items / do something larger for me / convert blocks / remove stuff?',
+        'question' => 'Can the admin give me items / do something larger for me / convert blocks / remove stuff?',
         'answer' => 'No, sorry, not possible. If I start doing that its first of all not fair towards others and second of all, I have better things to do.
             I spend enough time with admin tasks for you guys. Also the server motto is "build your dream" and not "have someone else build your dream.
             If you want to build crazy stuff without the work, you better go to a creative world instead of a survival world.',
         'categories' => array('commands'),
     ),
     15 => array(
-        'questions' => 'I have chosen the wrong lot! It’s all in the water! Can I change?',
-        'answers' => 'No. You cannot. You are supposed to go there first and check it out. I am not working extra to reward your laziness.
+        'question' => 'I have chosen the wrong lot! It’s all in the water! Can I change?',
+        'answer' => 'No. You cannot. You are supposed to go there first and check it out. I am not working extra to reward your laziness.
             Take it as a challenge and build something on the water.',
         'categories' => array('lots'),
     ),
     16 => array(
-        'questions' => 'How long does it take to get to higher level such as “Architect” or “Master”?',
+        'question' => 'How long does it take to get to higher level such as “Architect” or “Master”?',
         'answer' => 'This depends only on you. Those levels are voted on by other users. Make sure people see you and like what you do, and you will get a higher level in no time.',
         'categories' => array('userlevels'),
     ),
@@ -154,7 +156,7 @@ $FAQ = array(
     ),
     23 => array(
         'question' => 'I will be on holiday, will my lot reset?',
-        'answer' => 'YES. If you do not login within the reset time, it will reset and I will not do anyhting about it.',
+        'answer' => 'Yes. If you do not login within the reset time, it will reset and I will not do anyhting about it.',
         'categories' => array('lots'),
     ),
     24 => array(
@@ -175,17 +177,23 @@ $FAQ = array(
     ),
 );
 
-function umc_faq_web($id = 'accordion') {
-    global $FAQ;
-    $out = '<script>
-  $(function() {
-    $( "#accordion" ).accordion();
-  });
-  </script>';
+function umc_faq_web($id = 'accordion', $sel_id = 1) {
+    global $UMC_FAQ;
+
+    $active = $sel_id - 1;
+
+    $out = "<script>
+    jQuery(document).ready(function($) {
+        $( \"#$id\" ).accordion({
+            collapsible: true, heightStyle: \"content\", active: $active
+        });
+    });
+</script>";
 
     $out .= "<div id=\"$id\">\n";
-    foreach ($FAQ as $id => $F) {
-        $out .= "    <h3></h3>
+
+    foreach ($UMC_FAQ as $faq_id => $F) {
+        $out .= "    <h3>$faq_id: {$F['question']}</h3>
         <div>
             <p>{$F['answer']}</p>
         </div>";
