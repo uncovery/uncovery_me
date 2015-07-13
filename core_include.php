615ea7237daf515a5bc1eb988fdaaa233f2873b7
uncovery <minecraft@uncovery.me>
2015-07-13 14:59:29 +0800

    fixes

--- core_include.php
+++ core_include.php
@@ -270,34 +270,61 @@ function umc_print_truefalse($query) {
  * @return type
  */
 function umc_get_fcontent($url_raw, $javascript_loop = 0, $timeout = 5) {
-    $url = str_replace( "&amp;", "&", urldecode(trim($url_raw)) );
+    if (!is_array($url_raw)) {
+        $urls = array($url_raw);
+    }
 
-    $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1";
+    $channels = array();
+    foreach ($urls as $key => $url) {
+        $mh = curl_multi_init();
+        $url_fixed = str_replace( "&amp;", "&", urldecode(trim($url)));
 
-    $cookie = tempnam ("/tmp", "CURLCOOKIE");
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_ENCODING, "");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    # required for https urls
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-    $content = curl_exec($ch);
-    $response = curl_getinfo($ch);
-    curl_close ( $ch );
+        $user_agent = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0";
 
+        $cookie = tempnam("/tmp", "CURLCOOKIE");
+        $channels[$key] = curl_init();
+        curl_setopt_array($channels[$key], array(
+            // CURLOPT_USERAGENT => $user_agent,
+            CURLOPT_URL => $url_fixed,
+            CURLOPT_COOKIEJAR => $cookie,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "UTF-8",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_AUTOREFERER => true,
+            CURLOPT_SSL_VERIFYHOST => 2,
+            CURLOPT_SSL_VERIFYPEER => true, # required for https urls
+            CURLOPT_CONNECTTIMEOUT => $timeout,
+            CURLOPT_TIMEOUT => $timeout,
+            CURLOPT_MAXREDIRS => 10,
+        ));
+        curl_multi_add_handle($mh, $channels[$key]);
+    }
+
+    $active = null;
+    while ($active && $status == CURLM_OK) {
+        $status = curl_multi_exec($mh, $active);
+    }
+
+    $output = array();
+    foreach ($channels as $channel) {
+        $output[] = umc_get_fcontent_response_process($channel, $user_agent, $javascript_loop);
+        curl_multi_remove_handle($mh, $channel);
+        curl_close($channel);
+    }
+    curl_multi_close($mh);
+    return $output;
+}
+
+function umc_get_fcontent_response_process($channel, $user_agent, $javascript_loop) {
+    $content = curl_multi_getcontent($channel);
+    $response = curl_getinfo($channel);
     if ($response['http_code'] == 301 || $response['http_code'] == 302) {
         ini_set("user_agent", $user_agent);
         $headers = get_headers($response['url']);
         if ($headers) {
             foreach($headers as $value) {
                 if (substr(strtolower($value), 0, 9 ) == "location:") {
-                    return get_url( trim( substr( $value, 9, strlen($value) ) ) );
+                    return umc_get_fcontent(trim(substr($value, 9, strlen($value))));
                 }
             }
         }
@@ -305,8 +332,8 @@ function umc_get_fcontent($url_raw, $javascript_loop = 0, $timeout = 5) {
     $pattern_1 = "/>[[:space:]]+window\.location\.replace\('(.*)'\)/i";
     $pattern_2 = "/>[[:space:]]+window\.location\=\"(.*)\"/i";
     if ((preg_match($pattern_1, $content, $value) || preg_match($pattern_2, $content, $value)) && $javascript_loop < 5) {
-        return get_url( $value[1], $javascript_loop+1);
+        return umc_get_fcontent( $value[1], $javascript_loop+1);
     } else {
-        return array($content, $response);
+        return array('content' => $content, 'response' => $response);
     }
 }
