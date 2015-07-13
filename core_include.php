8a6a895aef0ae15ff8b920e68169705a7c47826d
uncovery <minecraft@uncovery.me>
2015-07-13 13:42:44 +0800

    add new function to download items using curl instead file_get_contents

--- core_include.php
+++ core_include.php
@@ -260,3 +260,53 @@ function umc_print_truefalse($query) {
         return 'false';
     }
 }
+
+/**
+ * Replacement function for file_get_contents
+ *
+ * @param type $url_raw
+ * @param type $javascript_loop
+ * @param type $timeout
+ * @return type
+ */
+function umc_get_fcontent($url_raw, $javascript_loop = 0, $timeout = 5) {
+    $url = str_replace( "&amp;", "&", urldecode(trim($url_raw)) );
+
+    $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1";
+
+    $cookie = tempnam ("/tmp", "CURLCOOKIE");
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_ENCODING, "");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    # required for https urls
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+    $content = curl_exec($ch);
+    $response = curl_getinfo($ch);
+    curl_close ( $ch );
+
+    if ($response['http_code'] == 301 || $response['http_code'] == 302) {
+        ini_set("user_agent", $user_agent);
+        $headers = get_headers($response['url']);
+        if ($headers) {
+            foreach($headers as $value) {
+                if (substr(strtolower($value), 0, 9 ) == "location:") {
+                    return get_url( trim( substr( $value, 9, strlen($value) ) ) );
+                }
+            }
+        }
+    }
+    $pattern_1 = "/>[[:space:]]+window\.location\.replace\('(.*)'\)/i";
+    $pattern_2 = "/>[[:space:]]+window\.location\=\"(.*)\"/i";
+    if ((preg_match($pattern_1, $content, $value) || preg_match($pattern_2, $content, $value)) && $javascript_loop < 5) {
+        return get_url( $value[1], $javascript_loop+1);
+    } else {
+        return array($content, $response);
+    }
+}
