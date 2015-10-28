d677e2e20d8c11447cc8525e33df3c22e4cf4d32
uncovery <minecraft@uncovery.me>
2015-10-28 14:40:09 +0800

    documentation & GPL header

--- websend_inc/info.inc.php
+++ websend_inc/info.inc.php
@@ -1,5 +1,26 @@
 <?php
+/*
+ * This file is part of Uncovery Minecraft.
+ * Copyright (C) 2015 uncovery.me
+ *
+ * Uncovery Minecraft is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/*
+ * This is a small plugin that send various information to the client. It also 
+ * allows password resets w/o using wordpress
+ */
 global $UMC_SETTING, $WS_INIT;
 
 $WS_INIT['info'] = array(  // the name of the plugin
