753bf5c8aed596a48c870e0b45535f7bbfd495f8
uncovery <minecraft@uncovery.me>
2016-01-05 14:57:46 +0800

    add comments

--- data/js/global.js
+++ data/js/global.js
@@ -18,9 +18,20 @@ jQuery(document).ready(function(jQuery) {
     jQuery('.umc_fade_in').fadeIn('fast');
 });
 
-/*
+// function to active the browser fingerprint
+// used by web.php umc_web_set_fingerprint()
+// variable stored in UUID table
+jQuery(document).ready(function(jQuery) {
+    var fp = new Fingerprint2();
+    fp.get(function(result) {
+        var fingerprint_url = "http://uncovery.me/admin/index.php?function=web_set_fingerprint&id=" + result;
+        jQuery.ajax(fingerprint_url);
+    });
+});
+
 // this should be in the footer to work
 // currently unused
+/*
 function umcAjaxFormProcess(destination, event) {
     jQuery('#umc_ajax_container').slideUp();
     jQuery('#umc_ajax_loading').slideDown();
@@ -42,6 +53,4 @@ jQuery("form input[type=submit]").click(function() {
         jQuery("input[type=submit]", $(this).parents("form")).removeAttr("clicked");
         jQuery(this).attr("clicked", "true");
 });
-
- * 
- */
+**/
