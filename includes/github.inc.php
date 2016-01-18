48182bad588014f1697f5af0226cff1aa40c9635
uncovery <minecraft@uncovery.me>
2016-01-18 11:23:43 +0800

    fix the fix

--- includes/github.inc.php
+++ includes/github.inc.php
@@ -23,7 +23,7 @@
  */
 global $GITHUB;
 $GITHUB['owner'] = 'uncovery';
-$GITHUB['repo'] = 'uncovery_me'; 
+$GITHUB['repo'] = 'uncovery_me';
 $GITHUB['page'] = 'http://uncovery.me/server-features/development-status/';
 $GITHUB['page_direct_issues'] = 'https://github.com/uncovery/uncovery_me/issues/';
 
@@ -100,7 +100,7 @@ function umc_github_link() {
         $parameters = array($owner, $repo, $I['state']);
         $$item = $paginator->fetchAll($api, $I['fetch'], $parameters);
     }
-    
+
     $parameters = array('sha' => 'master', 'per_page' => 100);
     $commits = $client->api('repo')->commits()->all($owner, $repo, $parameters);
 
@@ -132,13 +132,14 @@ function umc_github_link() {
              </table>";
     $o_count = count($open_issues);
     $c_count = count($closed_issues);
-    
+
     $out .= umc_jquery_tabs(array("Open Issues ($o_count)" =>$tab1, "Closed Issues ($c_count)"=>$tab2, 'Commits'=>$tab3));
 
     return $out;
 }
 
 function umc_github_issue_details($issue, $comments) {
+    global $GITHUB;
     $labels = '';
     foreach ($issue['labels'] as $label) {
         $labels .= " <span style='background-color: #{$label['color']}'>&nbsp;{$label['name']}&nbsp;</span> ";
@@ -186,22 +187,22 @@ function umc_github_issue_details($issue, $comments) {
 }
 
 function umc_github_wordpress_update() {
-    require_once('/home/minecraft/public_html/wp-load.php'); 
-    
+    require_once('/home/minecraft/public_html/wp-load.php');
+
     global $GITHUB;
     $repo = $GITHUB['repo'];
     $owner = $GITHUB['owner'];
     $page = $GITHUB['page'];
-    
+
     $client = umc_github_client_connect($owner, $repo);
-    
+
     $today_obj = new DateTime(NULL);
     $today_obj->modify('-1 day');
-    // $date_new->setTimezone(new DateTimeZone('Asia/Hong_Kong'));    
+    // $date_new->setTimezone(new DateTimeZone('Asia/Hong_Kong'));
     $today_str = $today_obj->format('Y-m-d\T16:00:00\Z');
-    
+
     $issue_arr = array();
-    
+
     $issues = $client->api('issue')->all($owner, $repo, array('state' => 'all', 'since' => $today_str));
     if (count($issues) == 0) {
         return;
@@ -213,7 +214,7 @@ function umc_github_wordpress_update() {
             $issue_updated_date = substr($issue['updated_at'], 0, 10);
             foreach ($issue['labels'] as $label) {
                 $labels .= "<span style='background-color: #{$label['color']}'>{$label['name']}</span>&nbsp;";
-            }            
+            }
             if (count($issue['labels']) > 0) {
                 $label_txt = " ($labels)";
             }
@@ -228,22 +229,23 @@ function umc_github_wordpress_update() {
                 $issue_arr['closed'][$issue['number']] = $text;
             }
         }
-    }    
+    }
 
     $out = "This is a daily update on the status of the work done behind the scenes. You can see the complete status <a href=\"$page\">here</a>.\n "
-        . "Our webserver is completely open source, hosted on GitHub. You can help improve the server by fixing issues <a href=\"https://github.com/uncovery/uncovery_me/issues">here</a>.<ul>\n"; 
+        . "Our webserver is completely open source, hosted on GitHub. You can help improve the server by fixing issues "
+        . "<a href=\"https://github.com/uncovery/uncovery_me/issues\">here</a>.\n<ul>\n";
     foreach ($issue_arr as $section => $lines) {
         $section_str = ucwords($section);
         $out .= "    <li><strong>Issues $section_str:</strong>\n";
-        $out .= "        <ul>\n"; 
+        $out .= "        <ul>\n";
         foreach ($lines as $line) {
             $out .= "            <li>$line</li>\n";
         }
-        $out .= "        </ul>\n"; 
+        $out .= "        </ul>\n";
         $out .= "    </li>\n";
     }
     $out .= "</ul>\n";
-    
+
     $post = array(
         'comment_status' => 'open', // 'closed' means no comments.
         'ping_status' => 'closed', // 'closed' means pingbacks or trackbacks turned off
@@ -253,5 +255,5 @@ function umc_github_wordpress_update() {
         'post_title' => "Today's development updates", //The title of your post.
         'post_type' => 'post' //You may want to insert a regular post, page, link, a menu item or some custom post type
     );
-    wp_insert_post($post); 
+    wp_insert_post($post);
 }
