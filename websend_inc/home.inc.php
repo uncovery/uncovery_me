2b1245fe566373bd2c1dcc797a2bdb66895f0224
uncovery <minecraft@uncovery.me>
2016-01-19 14:32:29 +0800

    concat change

--- websend_inc/home.inc.php
+++ websend_inc/home.inc.php
@@ -202,7 +202,7 @@ function umc_home_add($uuid, $name){
     $count = umc_home_count();
 
     // add a prefix string to lottery home name to prevent conflict
-    $newname = umc_random_code_gen() . '_' . $name;
+    $newname = $name . "_" . umc_random_code_gen();
     $userlevel = umc_get_uuid_level($uuid);
     $max_homes = $UMC_SETTING['max_homes'][$userlevel];
 
