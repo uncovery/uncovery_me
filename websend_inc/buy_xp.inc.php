6ec65c58ed3120d5a6f8e0fe537f67dbdbd95195
uncovery <minecraft@uncovery.me>
2016-01-27 11:58:32 +0800

    disable XP buying, rename plugin

--- websend_inc/buy_xp.inc.php
+++ websend_inc/buy_xp.inc.php
@@ -18,70 +18,71 @@
  */
 
 /*
- * This is a simple plugin that allows users to buy XP from the server 
+ * This is a simple plugin that allows users to buy XP from the server
  */
 
 global $UMC_SETTING, $WS_INIT;
 
-$WS_INIT['buyxp'] = array(  // the name of the plugin
+$WS_INIT['bottlexp'] = array(  // the name of the plugin
     'disabled' => false,
     'events' => false,
     'default' => array(
         'help' => array(
-            'title' => 'Buy XP',  // give it a friendly title
-            'short' => 'Buy XP for Uncs',  // a short description
-            'long' => "Buys XP for the value of <uncs>. The exchange rate is 1 Uncs per 1 XP.", // a long add-on to the short  description
+            'title' => 'Bottle XP',  // give it a friendly title
+            'short' => 'Transfer XP into bottles',  // a short description
+            'long' => "Tranfer XP from your user into bottles. The bottles are placed into your inventory.", // a long add-on to the short  description
             ),
     ),
-    'buyxp' => array( // this is the base command if there are no other commands
+    'bottlexp' => array( // this is the base command if there are no other commands
+        'help' => array(
+            'short' => 'Bottle XP',
+            'long' => "Bottles all current XP at the rate of 10 xp per bottle. There is a natural variance of XP from -70% to +10%.",
+        ),
+        'function' => 'umc_xp_bottle',
+        'security' => array(
+            'worlds' => array('empire', 'kingdom', 'skylands', 'aether', 'the_end'),
+        ),
+    ),
+    /*'buyxp' => array( // this is the base command if there are no other commands
         'help' => array(
             'short' => 'Buys XP',
             'long' => "Buys XP to the value of <Uncs>. The exchange rate is 1 Unc per 1 XP. Use /buyxp check to check your current XP levels.",
             'args' => '<Uncs>',
         ),
         'function' => 'umc_do_buyxp',
-    ),
-    'bottlexp' => array( // this is the base command if there are no other commands
-        'help' => array(
-            'short' => 'Bottle XP',
-            'long' => "Bottles all current XP at the rate of 10 xp per bottle.",
-        ),
-        'function' => 'umc_xp_bottle',
-        'security' => array(
-            'worlds' => array( 'empire', 'kingdom', 'skylands', 'aether', 'the_end'),
-    ),
+    ),*/
 );
 
 function umc_xp_bottle(){
-    
+
     global $UMC_USER;
-	
+
     $player = $UMC_USER['username'];
     $user_xp = $UMC_USER['xp'];
-    
+
     // make sure they have enough xp.
-    if ($user_xp < 10) {	
+    if ($user_xp < 10) {
         umc_error("{red}You need at least 10 XP points to bottle. You currently have only $user_xp.");
     }
-    
+
     // calculate
     $bottle_count = floor($user_xp / 10);
     $taking_xp = $bottle_count * 10;
     $uuid = umc_user2uuid($player);
-    $nex_xp = $user_xp - $taking_xp;
-    
+    $new_xp = $user_xp - $taking_xp;
+
     // set the nex exp value
     umc_ws_cmd("exp set $player $new_xp", 'asConsole');
-    
+
     // give the item into deposit
     umc_deposit_give_item($uuid, 393, 0, '', $bottle_count, 'bottlexp');
-    
+
     // create the log
     umc_log('buyxp', 'bottle', "$player bottled $taking_xp into $bottle_count bottles.");
-    
+
     // give some player feedback
     umc_echo("{green} You deposited $bottle_count bottles into your deposit box.");
-    
+
 }
 
 /**
@@ -92,38 +93,38 @@ function umc_xp_bottle(){
  */
 function umc_do_buyxp() {
     global $UMC_USER;
-	
+
     $player = $UMC_USER['username'];
     $args = $UMC_USER['args'];
     $xp_ratio = 1;
-	
+
     // check to see if player has entered a value of xp to buy
     if (isset($args[2])) {
-    	
+
     	// feedback on current xp point values
         $user_xp = $UMC_USER['xp'];
 	umc_echo("{white} You started with $user_xp experience points.");
-        
+
         // amount player is trying to spend
         $amount = $args[2];
 
         // cast argument to type int to sanitise the data
         settype($amount, 'int');
-	
+
         // amount of xp calculated
         $xp = floor($amount * $xp_ratio);
-        
+
         // retrieve the players balance to check if they can afford
         $balance = umc_money_check($player);
 
-        if ( $xp < 1 || $amount < 1 ) {	
+        if ( $xp < 1 || $amount < 1 ) {
             umc_error("{red}You need to buy at least 1 XP. For $amount Uncs you get only $xp XP (ratio is $xp_ratio!)");
         }
 
 	if ( $amount > $balance ) {
             umc_error("{red}Sorry, you cannot afford this purchase. You currently have $balance uncs.");
         }
-        
+
         // calculate the total new xp point value
         $new_xp = $user_xp + $xp;
 
@@ -134,16 +135,16 @@ function umc_do_buyxp() {
         // take the purchase amount from players account.
         // take from, give to, positive value
         umc_money($player, false, $amount);
-        
+
         // announce the purchase to encourage players to consider buying xp
         umc_announce("{gold}$player{gray} just bought {purple}$xp XP{gray} for{cyan} $amount Uncs{gray}!");
 	umc_echo("{white} You ended with $new_xp experience points.");
 
         // log the purchase
         umc_log('buyxp', 'buy', "$player paid $amount for $xp XP, going from $user_xp to $new_xp");
-    
+
     } else {
         umc_error("{red}You need to specify the amount of Uncs you want to spend. See {yellow}/helpme buyxp");
     }
-	
+
 }
