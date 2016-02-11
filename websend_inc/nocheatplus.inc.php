1fc7caa8e8463ebe28725e1c0c0fa084dec61d8a
uncovery <minecraft@uncovery.me>
2016-02-11 14:05:40 +0800

    implement JS Charts

--- websend_inc/nocheatplus.inc.php
+++ websend_inc/nocheatplus.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -19,17 +19,17 @@ function umc_nocheatplus_web() {
     foreach ($A as $row) {
         $drop_data[$row['action']] = ucwords($row['action']) . " (" . $row['counter'] . ")";
     }
-    
+
     $post_action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
     if (is_null($post_action)) {
         $action = 'passable';
     } else {
         $action = $post_action;
     }
-    
+
     $out = "<form action=\"\" method=\"post\">\n";
     $out .= umc_web_dropdown($drop_data, "action", $action, true);
-    
+
     $sql_action = umc_mysql_real_escape_string($action);
     $sql = "SELECT count(log_id) AS hit_count, DATE_FORMAT(`date`,'%Y-%u') AS date, sum(level)/count(log_id) as average
         FROM minecraft_log.nocheatplus
@@ -37,101 +37,14 @@ function umc_nocheatplus_web() {
         GROUP BY `action`, DATE_FORMAT(`date`,'%Y-%u')
         ORDER BY `date` ASC";
     $D = umc_mysql_fetch_all($sql);
-    
+
     $data_arr = array();
     foreach ($D as $d) {
-        $data_arr[$d['date']]["hit_count"] = $d['hit_count'];
-        $data_arr[$d['date']]["average"] = $d['average'];
-    }
-    
-    $out .= "\n<script type='text/javascript' src=\"$UMC_DOMAIN/admin/js/amcharts.js\"></script>\n"
-        . "<script type='text/javascript' src=\"$UMC_DOMAIN/admin/js/serial.js\"></script>\n"
-        . "<div id=\"chartdiv\" style=\"width: 100%; height: 400px;\"></div>\n"
-        . "<script type='text/javascript'>//<![CDATA[\n"
-        . "var chart;\n"
-        . "var chartData = [\n";
-    
-    $actions = array();
-    foreach ($data_arr as $date => $action_data) {
-        $out .= "{\"date\": \"$date\",";
-        foreach ($action_data as $action => $count) {
-            $out .= "\"$action\": $count,";
-            $actions[$action] = $action;
-        }
-        $out .= "},\n";
-    }
-    $out .= "];\n";
-
-    $out .= 'AmCharts.ready(function () {
-    // SERIAL CHART
-    chart = new AmCharts.AmSerialChart();
-    chart.pathToImages = "http://www.amcharts.com/lib/3/images/";
-    chart.dataProvider = chartData;
-    chart.marginTop = 10;
-    chart.categoryField = "date";
-
-    // AXES
-    // Category
-    var categoryAxis = chart.categoryAxis;
-    categoryAxis.gridAlpha = 0.07;
-    categoryAxis.axisColor = "#DADADA";
-    categoryAxis.startOnAxis = true;
-
-    // Value
-    var valueAxis = new AmCharts.ValueAxis();
-    // valueAxis.stackType = "regular"; // this line makes the chart "stacked"
-    valueAxis.gridAlpha = 0.07;
-    valueAxis.title = "Average";
-    valueAxis.id = "average";
-    valueAxis.position = "left";
-    chart.addValueAxis(valueAxis);
-    
-    var valueAxis = new AmCharts.ValueAxis();
-    // valueAxis.stackType = "regular"; // this line makes the chart "stacked"
-    valueAxis.gridAlpha = 0.07;
-    valueAxis.title = "Hitcount";
-    valueAxis.id = "hit_count";
-    valueAxis.position = "right";
-    chart.addValueAxis(valueAxis);';           
-
-    foreach ($actions as $action) {
-        $out .= "\nvar graph = new AmCharts.AmGraph();
-        graph.type = \"line\";
-        graph.hidden = false;
-        graph.title = \"$action\";
-        graph.valueField = \"$action\";
-        graph.valueAxis = \"$action\",
-        graph.lineAlpha = 1;
-        graph.fillAlphas = 0.6; // setting fillAlphas to > 0 value makes it area graph
-        graph.balloonText = \"<span style=\'font-size:12px; color:#000000;\'>$action: <b>[[value]]</b></span>\";
-        chart.addGraph(graph);";
+        $data_arr[$d['date']]["hitcount"] = $d['hit_count'];
+        $data_arr[$d['date']]["average"] = round($d['average']);
     }
 
-    $out .= '// LEGEND
-        var legend = new AmCharts.AmLegend();
-        legend.position = "top";
-        legend.valueText = "[[value]]";
-        legend.valueWidth = 100;
-        legend.valueAlign = "left";
-        legend.equalWidths = false;
-        legend.periodValueText = "total: [[value.sum]]"; // this is displayed when mouse is not over the chart.
-        chart.addLegend(legend);
-
-        // CURSOR
-        var chartCursor = new AmCharts.ChartCursor();
-        chartCursor.cursorAlpha = 0;
-        chart.addChartCursor(chartCursor);
-
-        // SCROLLBAR
-        var chartScrollbar = new AmCharts.ChartScrollbar();
-        chartScrollbar.color = "#FFFFFF";
-        chart.addChartScrollbar(chartScrollbar);
-
-        // WRITE
-        chart.write("chartdiv");
-        });
-        //]]></script>';  
-    
+    $out .= umc_web_javachart($data_arr, 'Weeks', 'none', array('hitcount' => 'left', 'average' => 'right'));
     $out .= "</form>";
     return $out;
 }
@@ -142,7 +55,7 @@ function umc_nocheatplus_logimport() {
     $file_path = $UMC_SETTING['nocheatplus']['logfile'];
 
     $regex = '/(^.{0,17}) \[INFO\] ([a-zA-Z_0-9]*) failed ([a-zA-Z_0-9]*):(.*)VL (\d*).$/';
-    
+
     $invalid_str = array(
         '[NoCheatPlus]',
         'settings could have changed',
@@ -193,9 +106,9 @@ function umc_nocheatplus_logimport() {
         $vl = umc_mysql_real_escape_string(trim($M[5]));
         // $sql_check = "SELECT count(log_id) as counter FROM minecraft_log.nocheatplus WHERE `date`=$date AND username=$username AND action=$action AND level=$vl;";
         // $C = umc_mysql_fetch_all($sql_check);
-        // if ($C[0]['counter'] < 1){ 
-            $sql = "INSERT INTO minecraft_log.nocheatplus(`date`, `username`, `action`, `level`, `text`) 
-                VALUES 
+        // if ($C[0]['counter'] < 1){
+            $sql = "INSERT INTO minecraft_log.nocheatplus(`date`, `username`, `action`, `level`, `text`)
+                VALUES
                 ($date,$username,$action,$vl,$text)";
             umc_mysql_execute_query($sql);
         // }
