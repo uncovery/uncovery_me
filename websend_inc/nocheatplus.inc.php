43607245303335a92043c9c50df82ec608b5f173
uncovery <minecraft@uncovery.me>
2016-02-12 13:50:19 +0800

    fix NCP file reading

--- websend_inc/nocheatplus.inc.php
+++ websend_inc/nocheatplus.inc.php
@@ -50,10 +50,13 @@ function umc_nocheatplus_web() {
 
 
 function umc_nocheatplus_logimport() {
+    XMPP_ERROR_trace(__FUNCTION__, func_get_args());
     global $UMC_SETTING;
     $file_path = $UMC_SETTING['nocheatplus']['logfile'];
 
     $regex = '/(^.{0,17}) \[INFO\] ([a-zA-Z_0-9]*) failed ([a-zA-Z_0-9]*):(.*)VL (\d*).$/';
+
+    $uuid_cache = array();
 
     $invalid_str = array(
         '[NoCheatPlus]',
@@ -65,9 +68,21 @@ function umc_nocheatplus_logimport() {
         '# Server #',
         '-Spigot-',
         'runs the command',
+        'Detected Minecraft version',
+        'Spigot',
+        'Added block-info for Minecraft',
+        'Inventory checks: FastConsume is available',
     );
-    $required_str = '[INFO]';
+    $required_str = '. VL '; // we need a violation level
     $line_count = 0;
+
+    // let's get the latest date from the database first
+    $latest_sql = "SELECT max(date) as max_date from minecraft_log.nocheatplus;";
+    $D = umc_mysql_fetch_all($latest_sql);
+    $max_date = $D[0]['max_date'];
+    $max_date_obj = new DateTime($max_date);
+    XMPP_ERROR_trace('max date:', $max_date);
+
     foreach (new SplFileObject($file_path) as $line) {
         $line_count ++;
         if (!strpos($line, $required_str)) {
@@ -99,10 +114,27 @@ function umc_nocheatplus_logimport() {
             break;
         }
         $date = umc_mysql_real_escape_string(trim($M[1]));
-        $uuid = umc_uuid_getone(strtolower(trim($M[2])), 'uuid', false);
-        if (!$uuid) {
+        $valid_date = '20' . str_replace(".", "-", trim($M[1]));
+        $date_obj = new DateTime($valid_date);
+        if ($date_obj < $max_date_obj) {
             continue;
         }
+
+        $username = strtolower(trim($M[2]));
+        if (!isset($uuid_cache[$username])) {
+            $uuid = umc_uuid_getone($username, 'uuid', true);
+            if (!$uuid) {
+                $uuid_cache[$username] = false;
+                continue;
+            }
+            $uuid_cache[$username] = $uuid;
+        } else if (!$uuid_cache[$username]) {
+            continue;
+        } else {
+            $uuid = $uuid_cache[$username];
+        }
+
+
         $uuid_sql = umc_mysql_real_escape_string($uuid);
         $action = umc_mysql_real_escape_string(strtolower(trim($M[3])));
         $text = umc_mysql_real_escape_string(trim($M[4]));
