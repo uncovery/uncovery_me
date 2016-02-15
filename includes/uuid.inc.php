60b1c7b51aee2d4947467fb84058469aaccd69fb
uncovery <minecraft@uncovery.me>
2016-02-15 11:10:20 +0800

    remove debug

--- includes/uuid.inc.php
+++ includes/uuid.inc.php
@@ -548,7 +548,6 @@ function umc_uuid_mojang_usernames($uuid) {
 
 function umc_uuid_username_history($uuid) {
     $previous_names = umc_uuid_check_history($uuid);
-    XMPP_ERROR_trigger($previous_names);
     if (count($previous_names) > 1) {
         $names = array();
         foreach ($previous_names as $name_data) {
