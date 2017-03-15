c43020420384de6eedb62b176a5d882d7a2fd654
uncovery <minecraft@uncovery.me>
2017-03-15 14:10:02 +0800

    typo

--- includes/websend.inc.php
+++ includes/websend.inc.php
@@ -434,7 +434,7 @@ function umc_ws_get_inv($inv_data) {
                 } else if (isset($UMC_DATA[$item_typename])) {
                     $inv[$slot]['item_name'] = $item_typename;
                 } else {
-                    $inv[$slot]['item_name'] = $UMC_DATA_ID2NAME[$item['Type']];
+                    $inv[$slot]['item_name'] = $UMC_DATA_ID2NAME[$item['TypeName']];
                     $out = "ITEM ISSUE! Please add: '$item_typename' => '{$inv[$slot]['item_name']}', to the \$UMC_DATA_SPIGOT2ITEM array";
                     XMPP_ERROR_send_msg($out);
                 }
