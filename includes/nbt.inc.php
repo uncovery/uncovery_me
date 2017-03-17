52de244da0d2060e39b2cd073b142bd25a1f73b5
uncovery <minecraft@uncovery.me>
2017-03-17 21:08:15 +0800

    insert NBT warning

--- includes/nbt.inc.php
+++ includes/nbt.inc.php
@@ -3,28 +3,32 @@
 /**
  * Fix the = into : for proper minecraft-valid NBT
  * The result can be used in /give etc
- * 
+ *
  * @param type $nbt_raw
  * @return type
  */
 function umc_nbt_cleanup($nbt_raw) {
     $regex = "/=(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/";
-    $meta_cmd = preg_replace($regex, ":", $nbt_raw);                
+    $meta_cmd = preg_replace($regex, ":", $nbt_raw);
     return $meta_cmd;
 }
 
 /**
  * Convert minecraft NBT to a valid JSON then to an array
- * 
+ *
  * @param type $nbt
  * @return type
  */
 function umc_nbt_to_array($nbt) {
+    XMPP_ERROR_trace(__FUNCTION__, func_get_args());
     // this regex basically takes all the array keys from the NBT data into $2 and puts quotes around them.
     $regex = '/([,{]{1,2})([^,}:]*):/';
-    $json = preg_replace($regex, '$1"$2":', $nbt);  
+    $json = preg_replace($regex, '$1"$2":', $nbt);
     // now we have valid json, decode it please
     $nbt_array = json_decode($json, true);
+    if (!$nbt_array) {
+        XMPP_ERROR_trigger("NBT Array invalid!");
+    }
     // we sort it so that same items with different order are displayed the same
     // I am not sure this is necessary though.
     // array_multisort($nbt_array);
@@ -33,10 +37,11 @@ function umc_nbt_to_array($nbt) {
 
 /**
  * takes an NBT string and converts it into something readable
- * 
+ *
  * @param type $nbt
  */
 function umc_nbt_display($nbt, $format) {
+    XMPP_ERROR_trace(__FUNCTION__, func_get_args());
     $nbt_array = umc_nbt_to_array($nbt);
     $formats = array(
         'long_text',
@@ -49,30 +54,36 @@ function umc_nbt_display($nbt, $format) {
     return $text;
 }
 
+/**
+ * pure text output with all details.
+ *
+ * @param type $nbt_array
+ * @return type
+ */
 function umc_nbt_display_long_text($nbt_array) {
     XMPP_ERROR_trace(__FUNCTION__, func_get_args());
     $text = '';
     foreach ($nbt_array as $feature => $data) {
         $feat = strtolower($feature);
         switch ($feat) {
-            case 'ench': 
+            case 'ench':
                 $text .= "Enchantments: ";
                 // example enchantment {ench:[{lvl:5,id:16},{lvl:5,id:17},{lvl:5,id:18},{lvl:2,id:19},{lvl:2,id:20},{lvl:3,id:21}]}
                 $enchs = array();
                 foreach ($data as $ench) {
                     // find the id in the enchantments data
                     $ench_name = umc_enchant_text_find('id', $ench['id'], 'name');
-                    $enchs[] = $ench_name . " Lvl {$ench['lvl']}"; 
+                    $enchs[] = $ench_name . " Lvl {$ench['lvl']}";
                 }
                 $text .= implode(", ", $enchs) . '\n';
                 break;
-            case 'display':               
+            case 'display':
                 if (isset($data['Name'])) {
                     $text .= "Called " . $data['Name'] . '\n';
                 }
                 if (isset($data['Lore'])) {
                     $text .= "Lore: " . implode('\n', $data['Lore']) . '\n';
-                }                
+                }
                 break;
             case 'repaircost':
                 $text .= "Repair Costs: $data". '\n';
@@ -116,7 +127,7 @@ function umc_nbt_display_long_text($nbt_array) {
                     $text .= implode(", ", $items) . '\n';
                 }
                 break;
-            case 'fireworks': 
+            case 'fireworks':
                 // {Fireworks:{Flight:2,Explosions:[{Type:1,Flicker:0,Trail:1,Colors:[11743532,5320730,8073150],FadeColors:[3887386,4312372,6719955]}]}}
                 $text .= $feature;
                 break;
