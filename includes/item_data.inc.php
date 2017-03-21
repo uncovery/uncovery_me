a84bf9c0384338ffffbe9fdea73f64957a5f93f0
uncovery <minecraft@uncovery.me>
2017-03-21 15:14:09 +0800

    better numbers for spawn egg types

--- includes/item_data.inc.php
+++ includes/item_data.inc.php
@@ -3032,39 +3032,39 @@ $UMC_DATA = array(
         'group' => 'spawn_egg_types',
         'icon_url' => '/f/fc/Grid_Spawn_Creeper.png',
         'nbt_types' => array(
-            29 => array('nbt' => '{EntityTag:{id:"minecraft:zombie_horse"}}', 'avail' => true, 'icon_url' => '/4/46/Undeadhorse.png'), //not sure if egg exists
-            31 => array('nbt' => '{EntityTag:{id:"minecraft:donkey"}}', 'avail' => true, 'icon_url' => '/9/95/Donkey.png'), //lottery
-            32 => array('nbt' => '{EntityTag:{id:"minecraft:mule"}}', 'avail' => true, 'icon_url' => '/e/e7/Mule.png'), //lottery
-            50 => array('nbt' => '{EntityTag:{id:"minecraft:creeper"}}', 'avail' => true, 'icon_url' => '/f/fc/Grid_Spawn_Creeper.png'),
-            51 => array('nbt' => '{EntityTag:{id:"minecraft:skeleton"}}', 'avail' => true, 'icon_url' => '/3/35/Grid_Spawn_Skeleton.png'),
-            52 => array('nbt' => '{EntityTag:{id:"minecraft:spider"}}', 'avail' => true, 'icon_url' => '/5/50/Grid_Spawn_Spider.png'),
-            54 => array('nbt' => '{EntityTag:{id:"minecraft:zombie"}}', 'avail' => true, 'icon_url' => '/3/30/Grid_Spawn_Zombie.png'),
-            55 => array('nbt' => '{EntityTag:{id:"minecraft:slime"}}', 'avail' => true, 'icon_url' => '/1/11/Grid_Spawn_Slime.png'),
-            56 => array('nbt' => '{EntityTag:{id:"minecraft:ghast"}}', 'avail' => true, 'icon_url' => '/3/3f/Grid_Spawn_Ghast.png'),
-            57 => array('nbt' => '{EntityTag:{id:"minecraft:pigman"}}', 'avail' => true, 'icon_url' => '/4/4d/Grid_Spawn_Zombie_Pigman.png'),
-            58 => array('nbt' => '{EntityTag:{id:"minecraft:enderman"}}', 'avail' => true, 'icon_url' => '/6/62/Grid_Spawn_Enderman.png'),
-            59 => array('nbt' => '{EntityTag:{id:"minecraft:cave_spider"}}', 'avail' => true, 'icon_url' => '/e/ee/Grid_Spawn_Cave_Spider.png'),
-            60 => array('nbt' => '{EntityTag:{id:"minecraft:silverfish"}}', 'avail' => true, 'icon_url' => '/1/11/Grid_Spawn_Silverfish.png'),
-            61 => array('nbt' => '{EntityTag:{id:"minecraft:blaze"}}', 'avail' => true, 'icon_url' => '/f/ff/Grid_Spawn_Blaze.png'),
-            62 => array('nbt' => '{EntityTag:{id:"minecraft:magma_cube"}}', 'avail' => true, 'icon_url' => '/2/25/Grid_Spawn_Magma_Cube.png'),
-            65 => array('nbt' => '{EntityTag:{id:"minecraft:bat"}}', 'avail' => true, 'icon_url' => '/f/f3/Grid_Spawn_Bat.png'),  //lottery
-            66 => array('nbt' => '{EntityTag:{id:"minecraft:witch"}}', 'avail' => true, 'icon_url' => '/7/7c/Grid_Spawn_Witch.png'),
-            67 => array('nbt' => '{EntityTag:{id:"minecraft:endermite"}}', 'avail' => true, 'icon_url' => '/a/af/Grid_Spawn_Endermite.png'),
-            68 => array('nbt' => '{EntityTag:{id:"minecraft:guardian"}}', 'avail' => true, 'icon_url' => '/5/5f/Grid_Spawn_Guardian.png'),
-            69 => array('nbt' => '{EntityTag:{id:"minecraft:shulker"}}', 'avail' => true, 'icon_url' => '/c/c7/Grid_Spawn_Shulker.png'),
-            90 => array('nbt' => '{EntityTag:{id:"minecraft:pig"}}', 'avail' => true, 'icon_url' => '/0/09/Grid_Spawn_Pig.png'), //lottery
-            91 => array('nbt' => '{EntityTag:{id:"minecraft:sheep"}}', 'avail' => true, 'icon_url' => '/f/f8/Grid_Spawn_Sheep.png'), //lottery
-            92 => array('nbt' => '{EntityTag:{id:"minecraft:cow"}}', 'avail' => true, 'icon_url' => '/8/80/Grid_Spawn_Cow.png'), //lottery
-            93 => array('nbt' => '{EntityTag:{id:"minecraft:chicken"}}', 'avail' => true, 'icon_url' => '/b/b0/Grid_Spawn_Chicken.png'), //lottery
-            94 => array('nbt' => '{EntityTag:{id:"minecraft:squid"}}', 'avail' => true, 'icon_url' => '/5/58/Grid_Spawn_Squid.png'), //lottery
-            95 => array('nbt' => '{EntityTag:{id:"minecraft:wolf"}}', 'avail' => true, 'icon_url' => '/4/4b/Grid_Spawn_Wolf.png'), //lottery
-            96 => array('nbt' => '{EntityTag:{id:"minecraft:mooshroom"}}', 'avail' => true, 'icon_url' => '/9/91/Grid_Spawn_Mooshroom.png'), //lottery
-            98 => array('nbt' => '{EntityTag:{id:"minecraft:ocelot"}}', 'avail' => true, 'icon_url' => '/e/e8/Grid_Spawn_Ocelot.png'), //lottery
-            100 => array('nbt' => '{EntityTag:{id:"minecraft:horse"}}', 'avail' => true, 'icon_url' => '/e/e0/Grid_Spawn_Horse.png'), //lottery
-            101 => array('nbt' => '{EntityTag:{id:"minecraft:rabbit"}}', 'avail' => true, 'icon_url' => '/3/33/Grid_Spawn_Rabbit.png'), //lottery
-            102 => array('nbt' => '{EntityTag:{id:"minecraft:polar_bear"}}', 'avail' => true, 'icon_url' => '/f/fe/PolarBear_Preview.png'), //lottery
-            103 => array('nbt' => '{EntityTag:{id:"minecraft:llama"}}', 'avail' => true, 'icon_url' => '/f/fe/PolarBear_Preview.png'), //lottery //TODO: find the right image
-            120 => array('nbt' => '{EntityTag:{id:"minecraft:villager"}}', 'avail' => true, 'icon_url' => '/0/02/Grid_Spawn_Villager.png'), //lottery
+            0 => array('nbt' => '{EntityTag:{id:"minecraft:zombie_horse"}}', 'avail' => true, 'icon_url' => '/4/46/Undeadhorse.png'), //not sure if egg exists
+            1 => array('nbt' => '{EntityTag:{id:"minecraft:donkey"}}', 'avail' => true, 'icon_url' => '/9/95/Donkey.png'), //lottery
+            2 => array('nbt' => '{EntityTag:{id:"minecraft:mule"}}', 'avail' => true, 'icon_url' => '/e/e7/Mule.png'), //lottery
+            3 => array('nbt' => '{EntityTag:{id:"minecraft:creeper"}}', 'avail' => true, 'icon_url' => '/f/fc/Grid_Spawn_Creeper.png'),
+            4 => array('nbt' => '{EntityTag:{id:"minecraft:skeleton"}}', 'avail' => true, 'icon_url' => '/3/35/Grid_Spawn_Skeleton.png'),
+            5 => array('nbt' => '{EntityTag:{id:"minecraft:spider"}}', 'avail' => true, 'icon_url' => '/5/50/Grid_Spawn_Spider.png'),
+            6 => array('nbt' => '{EntityTag:{id:"minecraft:zombie"}}', 'avail' => true, 'icon_url' => '/3/30/Grid_Spawn_Zombie.png'),
+            7 => array('nbt' => '{EntityTag:{id:"minecraft:slime"}}', 'avail' => true, 'icon_url' => '/1/11/Grid_Spawn_Slime.png'),
+            8 => array('nbt' => '{EntityTag:{id:"minecraft:ghast"}}', 'avail' => true, 'icon_url' => '/3/3f/Grid_Spawn_Ghast.png'),
+            9 => array('nbt' => '{EntityTag:{id:"minecraft:pigman"}}', 'avail' => true, 'icon_url' => '/4/4d/Grid_Spawn_Zombie_Pigman.png'),
+            10 => array('nbt' => '{EntityTag:{id:"minecraft:enderman"}}', 'avail' => true, 'icon_url' => '/6/62/Grid_Spawn_Enderman.png'),
+            11 => array('nbt' => '{EntityTag:{id:"minecraft:cave_spider"}}', 'avail' => true, 'icon_url' => '/e/ee/Grid_Spawn_Cave_Spider.png'),
+            12 => array('nbt' => '{EntityTag:{id:"minecraft:silverfish"}}', 'avail' => true, 'icon_url' => '/1/11/Grid_Spawn_Silverfish.png'),
+            13 => array('nbt' => '{EntityTag:{id:"minecraft:blaze"}}', 'avail' => true, 'icon_url' => '/f/ff/Grid_Spawn_Blaze.png'),
+            14 => array('nbt' => '{EntityTag:{id:"minecraft:magma_cube"}}', 'avail' => true, 'icon_url' => '/2/25/Grid_Spawn_Magma_Cube.png'),
+            15 => array('nbt' => '{EntityTag:{id:"minecraft:bat"}}', 'avail' => true, 'icon_url' => '/f/f3/Grid_Spawn_Bat.png'),  //lottery
+            16 => array('nbt' => '{EntityTag:{id:"minecraft:witch"}}', 'avail' => true, 'icon_url' => '/7/7c/Grid_Spawn_Witch.png'),
+            17 => array('nbt' => '{EntityTag:{id:"minecraft:endermite"}}', 'avail' => true, 'icon_url' => '/a/af/Grid_Spawn_Endermite.png'),
+            18 => array('nbt' => '{EntityTag:{id:"minecraft:guardian"}}', 'avail' => true, 'icon_url' => '/5/5f/Grid_Spawn_Guardian.png'),
+            19 => array('nbt' => '{EntityTag:{id:"minecraft:shulker"}}', 'avail' => true, 'icon_url' => '/c/c7/Grid_Spawn_Shulker.png'),
+            20 => array('nbt' => '{EntityTag:{id:"minecraft:pig"}}', 'avail' => true, 'icon_url' => '/0/09/Grid_Spawn_Pig.png'), //lottery
+            21 => array('nbt' => '{EntityTag:{id:"minecraft:sheep"}}', 'avail' => true, 'icon_url' => '/f/f8/Grid_Spawn_Sheep.png'), //lottery
+            22 => array('nbt' => '{EntityTag:{id:"minecraft:cow"}}', 'avail' => true, 'icon_url' => '/8/80/Grid_Spawn_Cow.png'), //lottery
+            23 => array('nbt' => '{EntityTag:{id:"minecraft:chicken"}}', 'avail' => true, 'icon_url' => '/b/b0/Grid_Spawn_Chicken.png'), //lottery
+            24 => array('nbt' => '{EntityTag:{id:"minecraft:squid"}}', 'avail' => true, 'icon_url' => '/5/58/Grid_Spawn_Squid.png'), //lottery
+            25 => array('nbt' => '{EntityTag:{id:"minecraft:wolf"}}', 'avail' => true, 'icon_url' => '/4/4b/Grid_Spawn_Wolf.png'), //lottery
+            26 => array('nbt' => '{EntityTag:{id:"minecraft:mooshroom"}}', 'avail' => true, 'icon_url' => '/9/91/Grid_Spawn_Mooshroom.png'), //lottery
+            27 => array('nbt' => '{EntityTag:{id:"minecraft:ocelot"}}', 'avail' => true, 'icon_url' => '/e/e8/Grid_Spawn_Ocelot.png'), //lottery
+            28 => array('nbt' => '{EntityTag:{id:"minecraft:horse"}}', 'avail' => true, 'icon_url' => '/e/e0/Grid_Spawn_Horse.png'), //lottery
+            29 => array('nbt' => '{EntityTag:{id:"minecraft:rabbit"}}', 'avail' => true, 'icon_url' => '/3/33/Grid_Spawn_Rabbit.png'), //lottery
+            30 => array('nbt' => '{EntityTag:{id:"minecraft:polar_bear"}}', 'avail' => true, 'icon_url' => '/f/fe/PolarBear_Preview.png'), //lottery
+            31 => array('nbt' => '{EntityTag:{id:"minecraft:llama"}}', 'avail' => true, 'icon_url' => '/f/fe/PolarBear_Preview.png'), //lottery //TODO: find the right image
+            32 => array('nbt' => '{EntityTag:{id:"minecraft:villager"}}', 'avail' => true, 'icon_url' => '/0/02/Grid_Spawn_Villager.png'), //lottery
         ),
     ),
     'experience_bottle' => array(
