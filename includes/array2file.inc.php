59ed8bfbe9c5ac49a57a32f51221df615a59ccab
uncovery <minecraft@uncovery.me>
2017-05-11 18:26:19 +0800

    remove debug

--- includes/array2file.inc.php
+++ includes/array2file.inc.php
@@ -26,7 +26,6 @@
  * @return string
  */
 function umc_array2file($data, $array_name, $file) {
-    XMPP_ERROR_trace(__FUNCTION__, func_get_args());
     $array_name_upper = strtoupper($array_name);
     $out = '<?php' . "\n"
         . '$' . $array_name_upper. " = array(\n"
@@ -37,13 +36,12 @@ function umc_array2file($data, $array_name, $file) {
 }
 
 function umc_array2file_line($array, $layer, $val_change_func = false) {
-    XMPP_ERROR_trace(__FUNCTION__, func_get_args());
     $in_text = umc_array2file_indent($layer);
     $out = "";
     foreach ($array as $key => $value) {
         if ($val_change_func) {
             $value = $val_change_func($key, $value);
-        }        
+        }
         $out .=  "$in_text'$key' => ";
         if (is_array($value)) {
             $layer++;
