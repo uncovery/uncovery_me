2308d82af79a8281c650d6ef7c43c00b99751726
Uncovery <uncovery@users.noreply.github.com>
2018-04-18 15:07:43 +0900

    fix function name

--- includes/wordpress.inc.php
+++ includes/wordpress.inc.php
@@ -57,7 +57,7 @@ function umc_wp_get_vars() {
             $UMC_USER['email'] = $user_email;
             $UMC_USER['username'] = umc_uuid_getone($uuid, 'username');
             $UMC_USER['uuid'] = $uuid;
-            $UMC_USER['userlevel'] = umc_get_uuid_level($uuid);
+            $UMC_USER['userlevel'] = umc_userlevel_get($uuid);
             if (strstr($UMC_USER['userlevel'], 'Donator')) {
                 $UMC_USER['donator'] = 'Donator';
             } else {
