7f717086bab03988dbd2f9c4effdcb86bce4c762
Uncovery <uncovery@users.noreply.github.com>
2018-04-18 12:29:23 +0900

    userlevel function refactoring

--- commands/map_make.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include('/home/minecraft/server/bin/index_wp.php');
-
-umc_assemble_maps();
-// umc_create_blank_heatmap();
-
-?>
--- commands/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once('/home/minecraft/server/bin/index_wp.php');
-umc_restore_from_backup();
-
