bde29f66836b328a75ff659e12fe9e0bf4a064d3
Uncovery <minecraft@uncovery.me>
2019-02-20 09:57:59 +0800

    remove old icon creation scheduler

--- commands/schedule_pre_reboot.php
+++ commands/schedule_pre_reboot.php
@@ -6,24 +6,27 @@ XMPP_ERROR_trigger("Running schedule_pre_reboot!");
 
 umc_log('system', 'daily_process', "pre-reboot processes started");
 
+umc_log('system', 'daily_process', "running server_pre_reboot");
 umc_plugin_eventhandler('server_pre_reboot');
 
 // reset all user lots
 umc_lot_reset_process();
 
 // reset user lot count in UUID table
+umc_log('system', 'daily_process', "running umc_uuid_record_lotcount");
 umc_uuid_record_lotcount();
 
+umc_log('system', 'daily_process', "running umc_usericon_get");
 umc_usericon_get();
 
+umc_log('system', 'daily_process', "running umc_github_wordpress_update");
 umc_github_wordpress_update();
 
-umc_item_data_id2namelist();
-
-umc_item_data_icon_getdata();
-
+umc_log('system', 'daily_process', "running umc_item_fix_old");
 // rename old items in the databases to the new names (item_data.inc.php)
 umc_item_fix_old();
 
+umc_log('system', 'daily_process', "running umc_plg_enum");
 umc_plg_enum();
 
+umc_log('system', 'daily_process', "pre-reboot processes done!");
